1a8aafaaa848a01505b8c8d15ba305d4fc53f179
Jonas Wielicki <j.wielicki@sotecware.net>
2013-08-04 19:09:30 +0200

    Add missing #ifdef'd DEBUG_READ

--- utils.c
+++ utils.c
@@ -176,7 +176,9 @@ ssize_t block_read(int fd, void *buf, size_t count)
     while (cur < end) {
         ssize_t read_bytes = read(fd, cur, end - cur);
         if (read_bytes < 0) {
+#ifdef DEBUG_READ
             printf("' !error");
+#endif
             break;
         }
 #ifdef DEBUG_READ
